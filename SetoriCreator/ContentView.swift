48552999a01cd39b8635de4a611bb9c23ce51594
sattsat <syuuatit141offer@gmail.com>
2024-07-30 11:38:48 +0900

    Initial Commit

---
+++ SetoriCreator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetoriCreator
+//
+//  Created by 石井大翔 on 2024/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetoriCreator/SetoriCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetoriCreatorApp.swift
+//  SetoriCreator
+//
+//  Created by 石井大翔 on 2024/07/30.
+//
+
+import SwiftUI
+
+@main
+struct SetoriCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
